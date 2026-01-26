6f42a596d816514ce58f42a11a24b867c619ac17
Alnieto7 <05532@smkrus.sch.id>
2026-01-26 18:47:27 +0700

    [AO] media query responsive

--- lib/pages/siswa_page.dart
+++ lib/pages/siswa_page.dart
@@ -27,58 +27,60 @@ class SiswaPage extends StatelessWidget {
         centerTitle: true,
         elevation: 4,
       ),
-      body: Obx(() {
-        return ListView.builder(
-          padding: const EdgeInsets.all(12),
-          itemCount: siswaController.siswa.length,
-          itemBuilder: (context, index) {
-            Siswa siswa = siswaController.siswa[index];
+      body: Center(
+        child: SizedBox(
+          width: siswaController.contentWidth,
+          child: Obx(() {
+            return ListView.builder(
+              padding: const EdgeInsets.all(12),
+              itemCount: siswaController.siswa.length,
+              itemBuilder: (context, index) {
+                Siswa siswa = siswaController.siswa[index];
 
-            return Card(
-              margin: const EdgeInsets.symmetric(vertical: 8),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ListTile(
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 16,
-                ),
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
+                    ),
 
-                title: CustomText(
-                  myText: siswa.name,
-                  myTextColor: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  align: TextAlign.start,
-                ),
+                    title: CustomText(
+                      myText: siswa.name,
+                      myTextColor: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      align: TextAlign.start,
+                    ),
 
-                subtitle: CustomText(
-                  myText: "Absen: ${siswa.absen} | Makanan: ${siswa.makanan}",
-                  myTextColor: Colors.grey[800]!,
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                  align: TextAlign.start,
-                ),
+                    subtitle: CustomText(
+                      myText:
+                          "Absen: ${siswa.absen} | Makanan: ${siswa.makanan}",
+                      myTextColor: Colors.grey[800]!,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      align: TextAlign.start,
+                    ),
 
-                trailing: const Icon(
-                  Icons.edit,
-                  color: Colors.deepPurple,
-                ),
+                    trailing: const Icon(Icons.edit, color: Colors.deepPurple),
 
-                onTap: () {
-                  Get.toNamed(AppRoutes.edit_page,
-                    arguments: {
-                      'siswa': siswa,
+                    onTap: () {
+                      Get.toNamed(
+                        AppRoutes.edit_page,
+                        arguments: {'siswa': siswa},
+                      );
                     },
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             );
-          },
-        );
-      }),
+          }),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple,
         child: const Icon(Icons.add, color: Colors.white),
